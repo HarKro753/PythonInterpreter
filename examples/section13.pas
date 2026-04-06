4a015fada97f535ca5c6966f594c535d95254bbc
Harro Krog <krogharro@gmail.com>
2026-04-06 22:59:43 +0200

    remove

--- examples/section13.pas
+++
@@ -1,9 +0,0 @@
-PROGRAM Part13;
-VAR
-    x : INTEGER;
-    y : REAL;
-
-BEGIN
-    x := 2;
-    y := x + 3.5
-END.
--- examples/section9.pas
+++
@@ -1,9 +0,0 @@
-BEGIN
-    x := 2;
-    y := 3;
-    z := x + y * 2;
-    BEGIN
-        a := z + 1
-    END;
-    b := a - x
-END.
